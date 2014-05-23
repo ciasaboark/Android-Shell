86daaf8e2367b7f012847f720b5c9807de97e42e
Christopher Hagler <haglerchristopher@gmail.com>
2014-05-23 02:52:51 -0400

    Ls now prints off the contents of directories without crashing the app. Still need to change so it can print of file permissions and inodes

--- src/com/example/com/programmingthetux/commands/Ls.java
+++ src/com/example/com/programmingthetux/commands/Ls.java
@@ -1,6 +1,8 @@
 package com.example.com.programmingthetux.commands;
 
 import java.io.File;
+import java.io.FilePermission;
+import java.security.AccessController;
 
 import com.example.com.programmingthetux.tutorial.MainActivity;
 
@@ -11,35 +13,53 @@ public class Ls extends Command {
 	}
 	
 	@Override
+	/*
+	 * If more than one directory is given, just list the contents of the first one 
+	 * and discard the second. Further more if no director is given at all just diplay the files 
+	 * in the present working directory.
+	 * 
+	 * TO-DO: Ls is only displaying the contents of a directory. Still need to add the options for file 
+	 * permissions and inodes. 
+	 */
 	public int execute(MainActivity ctx, String[] parameters) {
 		File file;
-		File[] list;
+		File[] list = null;
 		
 		int returnCode = 1;
-		if (parameters.length == 0) {
+		if (parameters.length == 0) { //list the current directory
 			file = new File(ctx.getCurWrkDir());
 			list = file.listFiles();
-			
-			if(list.length > 0) {
-				for(int i = 0; i < list.length; i++) {
-					ctx.appendOutput(list[i].toString());
-				}
-			}
-			else if(list.length > 0) {
-				file = new File(parameters[0]);
-				
-				if (file != null) {
-					list = file.listFiles();
-					
-					if(list.length > 0) {
-						for(int i = 0; i < list.length; i++) {
-							ctx.appendOutput(list[i].toString());
-						}
+			try {
+				if(list.length > 0) {
+					for(int i = 0; i < list.length; i++) {
+						ctx.appendOutput(list[i].toString());
 					}
 				}
+			} catch (Exception e) { 
+				ctx.appendOutput("insufficient privileges");
 			}
-		}
-		return 0;
+		} else if(parameters.length > 0) {
+				file = new File(parameters[0]);
+				if(file.isDirectory()) {
+					try {
+						if (file != null) {
+						list = file.listFiles();
+					
+							if(list.length > 0) {
+								for(int i = 0; i < list.length; i++) {
+								ctx.appendOutput(list[i].toString());
+								}
+							}
+						}
+					} catch (Exception e) {
+						ctx.appendOutput("insufficient privileges");
+					}
+				}
+				else {
+					ctx.appendOutput("No such directory exits");
+				}					
+			}
+		return returnCode;
 	}
 
 	@Override
